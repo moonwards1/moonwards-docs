45b9907223dc0a7559c991066fcb067293d9c7ee
Yael Atletl <yaelatletl@gmail.com>
2019-06-17 22:01:39 -0500

    Autodoc ready

--- tools/gdscript_to_rst.py
+++ tools/gdscript_to_rst.py
@@ -11,8 +11,8 @@ keywords = ['signal', 'var', 'export', 'func', 'extends']
 def get_size_for(list):
     max = 0
     for elements in list:
-        if elements.len()>max:
-            max=elements.len()
+        if len(elements)>max:
+            max=len(elements)
     return max
 
 
@@ -34,40 +34,50 @@ def main(argv):
             outputfile = arg
 
     head, tail = os.path.split(inputfile)
+    tail = tail[0:-3]
     with open(inputfile,"r") as input_file:
         print("Input file open!!")
         line = input_file.readline()
         while line != '':
+            if line.find("#")!=-1:
+                line=line[0:line.find("#")]
             if any(keyword in line for keyword in keywords):
-                if 'extends' in line:
+
+                if 'extends ' in line:
                     extends = line
-                if keyword == 'signal':
-                    signal_lines.append(line)
-                if keyword == 'var':
+                    #continue
+                if 'signal ' in line:
+                    if not 'emit_signal' in line:
+                        signal_lines.append(line)
+                        #continue
+                if 'var ' in line:
                     variable_lines.append(line)
-                if keyword == 'export':
+                #    continue
+                if 'export ' in line:
                     property_lines.append(line)
-                if keyword == 'func':
+                #    continue
+                if 'func ' in line:
                     method_lines.append(line)
+                #    continue
             line = input_file.readline()
 
-    with open(outputfile,"rw") as output_file:
+    with open(outputfile,"w+") as output_file:
         print("Output File opened!!")
         output_file.write(tail+"\n")
-        for i in range(0,tail.len()):
+        for i in range(0,len(tail)):
             output_file.write("=")
-        output_file.write("\n")
+        output_file.write("\n\n")
         extends.replace('extends', '')
-        output_file.write("**Inherits:**" + ":godot_class:`"+extends + "`\n")
-        output_file.write("**Category:** <FILL THIS SPACE>\n")
+        output_file.write("**Inherits:**" + ":godot_class:`"+extends.rstrip(' \n')+ "`\n\n")
+        output_file.write("**Category:** <FILL THIS SPACE>\n\n")
         output_file.write("Brief Description\n-----------------\n")
-        output_file.write("\n<FILL A BRIEF DESCRIPTION HERE>\n")
+        output_file.write("\n<FILL A BRIEF DESCRIPTION HERE>\n\n")
         output_file.write("Methods\n-------\n")
 
 # This block of code will format the methods #
 
-        for line in method_lines:
-            line.replace('func ', '')
+        for index in range(0,len(method_lines)):
+            method_lines[index] = method_lines[index].replace('func ', '')
             #line = "**" + line + "**" + " **(** "+ line[line.find("(")+1:line.find(")")] + "**)**"
 
 # Ends the method formatting
@@ -87,13 +97,13 @@ def main(argv):
             output_file.write("+\n|")
             for times in range(0,54):
                 output_file.write(" ")
-            output_file.write("| ")
-            to_write = ":ref:`" + line[0:line.find("(")-1] + "<"
-            to_write = to_write + tail + "_method_"+line[0:line.find("(")-1]+">`"
-            to_write = to_write + " **(** " + line[line.find("(")+1:line.find(")")-1] + " **)** "
+            output_file.write(" | ")
+            to_write = ":ref:`" + line[0:line.find("(")] + "<"
+            to_write = to_write + tail + "_method_"+line[0:line.find("(")]+">`"
+            to_write = to_write + " **(** " + line[line.find("(")+1:line.find(")")] + " **)** "
             output_file.write(to_write) #Writes a reference to descriptions
 
-            for times in range(0,final_line_lenght-to_write.len()-1):
+            for times in range(0,final_line_lenght-len(to_write)-1):
                 output_file.write(" ")
             output_file.write("|\n")
 
@@ -109,30 +119,30 @@ def main(argv):
 
 # At this point the table has been completely written
 
-        output_file.write("Signals\n-------\n\n")
+        output_file.write("\nSignals\n-------\n\n")
 
 # Here starts the signal formatting #
-        for line in signal_lines:
-            line.replace('signal ', '')
+        for index in range(0,len(signal_lines)):
+            signal_lines[index] = signal_lines[index].replace('signal ', '')
 
         for line in signal_lines:
             if line.find("(")!=-1:
-                line_to_write = "**" + line + "**"
-                line_to_write = line_to_write + " **(** " + line[line.find("(")+1:line.find(")")-1] + " **)** "
-                output_file.write(".. _"+tail+"_signal_"+line[0:line.find("(")]+":\n\n")
+                line_to_write = "**" + line[0:line.find("(")].rstrip(' \n') + "**"
+                line_to_write = line_to_write + " **(** " + line[line.find("(")+1:line.find(")")].rstrip(' \n') + " **)** "
+                output_file.write(".. _"+tail+"_signal_"+line[0:line.find("(")].rstrip(' \n')+":\n\n")
                 output_file.write("- "+line_to_write+"\n\n")
             else:
-                output_file.write(".. _"+tail+"_signal_"+line+":\n\n")
-                output_file.write("- **" + line + "** **(** **)**\n\n")
+                output_file.write(".. _"+tail+"_signal_"+line.rstrip(' \n')+":\n\n")
+                output_file.write("- **" + line.rstrip(' \n') + "** **(** **)**\n\n")
             output_file.write("!<FILL DESCRIPTION HERE>!\n\n")
 
         output_file.write("Description\n-----------\n\n!<FILL DESCRIPTION HERE>!\n\n")
         output_file.write("Method Descriptions\n-------------------\n\n")
 
         for line in method_lines:
-            output_file.write(".. _"+tail+"_method_"+line[0:line.find("(")-1]+":\n\n")
-            line_to_write = "- :godot_class:`Title <FILL>` **" + line[0:line.find("(")-1]
-            line_to_write = line_to_write + "** **(** "+line[line.find("("):line.find(")")]+" **)**\n\n"
+            output_file.write(".. _"+tail+"_method_"+line[0:line.find("(")]+":\n\n")
+            line_to_write = "- :godot_class:`Title <FILL>` **" + line[0:line.find("(")]
+            line_to_write = line_to_write + "** **(** "+line[line.find("(")+1:line.find(")")]+" **)**\n\n"
             output_file.write(line_to_write)
             output_file.write("!<FILL DESCRIPTION HERE>!\n\n")
 
